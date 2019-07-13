b4e2412869140c32068c545c00442b985b9a2072
Cody Shaben <cshaben87@gmail.com>
2019-07-13 15:41:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   seperate_words = sentence1.split(" ")
-  reverse_words = seperate_words.each do |seperate_words|
+  reverse_words = seperate_words.map do |seperate_words|
     "#{seperate_words.reverse}"
   end
   reverse_words = reverse_words.join(" ")
