917969d7792cd3def4f258a6639571b46c474c4f
Cody Shaben <cshaben87@gmail.com>
2019-07-13 15:29:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?".split
-  
+  reversed_words = reverse_each_word.map do |reverse_each_word|
+    "#{reverse_each_word."
 end
